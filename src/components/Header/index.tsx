cbc93852206003fa0d6408df72c0533a93dc9d03
cdfortes <cdfortes@gmail.com>
2022-06-09 05:30:54 -0100

    sidebar complete2

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,10 @@
+import styles from './index.module.css'
+import igniteLogoImg from '../../assets/logo.svg'
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <img src={igniteLogoImg} alt="Logo do Ignite" />
+    </header>
+  )
+}
